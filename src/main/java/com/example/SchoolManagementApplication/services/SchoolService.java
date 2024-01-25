4bc48676c1cd0b32cb55d10c83aadc5cfc9ec45e
omoma ig <omomaighawosa440@gmail.com>
2024-01-25 16:53:30 +0100

    set up backend

---
+++ src/main/java/com/example/SchoolManagementApplication/services/SchoolService.java
@@ -0,0 +1,29 @@
+package com.example.SchoolManagementApplication.services;
+
+import java.util.List;
+
+
+import com.example.SchoolManagementApplication.entity.School;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface SchoolService {
+
+    public void create(String name, String address, int no_of_staff);
+
+    public School getSchool(Long id);
+
+    public void updateSchool(School school);
+
+    public void deleteSchool(long id);
+
+    public void deleteBulkSchools(List<Long> ids);
+
+
+    public Page<School> getAllSchool(int pageNumber, int pageSize);
+
+    public Page<School> getSchoolsByName(School name, int pageNumber, int pageSize);
+
+
+}
